343991eda45f6804ece10fbaeb2941173bbfd5c8
[Ritik] <ritikkumar8077008067@gmail.com>
2025-12-05 20:41:09 +0530

    arg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./Pages/Header/Header";
 import Footer from "./Pages/Footer/Footer";
-import Home from "./pages/home/Home";
+import Home from "./Pages/Home/Home";
 import './fonts.css'
 
 
